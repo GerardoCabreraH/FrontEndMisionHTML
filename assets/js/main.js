dc80a0fd7a81b943d2a3b9cabbecdfa7a0545672
Gerardo Rafael Cabrera Herrera <gecabrera@outlook.com>
2022-03-02 17:18:11 -0600

    Crear panel de administrador

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,15 @@ function totalIt() {
     }
     document.getElementsByName("total")[0].value = "$" + total.toFixed(2);
 }
+
+function showSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var dashboard = document.getElementById("dashboard");
+    if (sidebar.style.display === "none" && dashboard.style.display === "block") {
+        sidebar.style.display = "block";
+        dashboard.style.display = "none";
+    } else {
+        sidebar.style.display = "none";
+        dashboard.style.display = "block";
+    }
+}
